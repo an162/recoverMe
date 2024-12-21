f2ca991d156066ce5de915b51ceb328331f86cfd
melynda <melynda2097@gmail.com>
2024-12-21 19:05:41 +0100

    lib folder of homepage with db and bloc

--- recover_me/android/app/src/main/kotlin/com/example/recover_me/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.recover_me
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- recover_me/lib/activity.dart
+++
@@ -1,7 +0,0 @@
-class Activity {
-  final int points;
-  final DateTime date;
-  final String description;
-
-  Activity({required this.points, required this.date, required this.description});
-}
